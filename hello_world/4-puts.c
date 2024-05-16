b781a8ee21a13a3479bd701ac603e7588963acbe
AmbarRenata <ambarrenata5@gmail.com>
2024-05-16 23:47:23 +0000

    4-puts.c

--- hello_world/4-puts.c
+++ hello_world/4-puts.c
@@ -3,12 +3,10 @@
 /**
  * main - prints "Programming is like building a multilingual
  * puzzle, followed by a newline"
- * return 0.
+ * Return: Always 0.
  */
-
 int main(void)
 {
-    puts("\"Programming is like building a multilingual puzzle");
- 
-    return(0);
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
 }
