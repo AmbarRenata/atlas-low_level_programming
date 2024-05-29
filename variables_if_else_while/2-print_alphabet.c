a7c8a3da487607bb4f57dcd470729b18e9f5ae8f
AmbarRenata <ambarrenata5@gmail.com>
2024-05-29 23:36:27 +0000

    2-print_alphabet.c

---
+++ variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ * main - prints the alphabet in lowercase, followed by a new line
+ * Return: Always 0
+ */
+
+int main(void)
+
+{
+	char n;
+
+	for (n = 'a'; n <= 'z'; n++)
+	putchar(n);
+	putchar('\n');
+	return (0);
+}
